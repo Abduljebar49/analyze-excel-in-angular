ff36fc5357b268f1aaa1297f700878bed587071d
Abduljebar Sani <Abduljebar49@gmail.com>
2023-07-14 18:26:27 +0300

    excel read added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'analyze-excel-in-angular';
+  excelData: any[] = [];
+  excelHeader = []
+  fileName: string = "";
+  p: number = 1;
+  isFileUploading: boolean = false;
+
+  getFileName(file: any) {
+    var name = file.name.toString().split('.');
+    this.fileName = name[0];
+  }
+
+  onStockStatusFileChange($event: any) {
+    this.excelData = [];
+    this.getFileName($event.target.files[0]);
+    this.excelHeader = [];
+    this.excelData = [];
+    const target: DataTransfer = <DataTransfer>$event.target;
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { raw: false, type: 'binary' });
+      const wsname: string = wb.SheetNames[0];
+
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      this.excelData = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      this.excelHeader = this.excelData[0];
+      this.excelData = this.excelData.slice(1);
+      this.checkStockStatusCompatibility();
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+  checkStockStatusCompatibility() {
+
+  }
+
+  uploadStockStatus() {
+    this.isFileUploading = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPaginationModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
